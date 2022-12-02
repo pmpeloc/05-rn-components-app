57675eb4720aee43f52800386ddf6d967b6954f6
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-12-02 09:19:13 -0300

    feat: Easing and Bounce added

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,15 +1,22 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, { useRef } from 'react';
-import { View, StyleSheet, Animated, Button } from 'react-native';
+import { View, StyleSheet, Animated, Button, Easing } from 'react-native';
 
 export const Animation101Screen = () => {
   const opacity = useRef(new Animated.Value(0)).current;
+  const top = useRef(new Animated.Value(-100)).current;
 
   const fadeIn = () => {
     Animated.timing(opacity, {
-      toValue: 0.4,
+      toValue: 1,
       duration: 300,
       useNativeDriver: true,
+    }).start();
+    Animated.timing(top, {
+      toValue: 0,
+      duration: 800,
+      useNativeDriver: true,
+      easing: Easing.bounce,
     }).start();
   };
 
@@ -24,7 +31,12 @@ export const Animation101Screen = () => {
   return (
     <View style={styles.container}>
       <Animated.View
-        style={{ ...styles.purpleBox, marginBottom: 20, opacity }}
+        style={{
+          ...styles.purpleBox,
+          marginBottom: 20,
+          opacity,
+          transform: [{ translateY: top }],
+        }}
       />
       <Button title="FadeIn" onPress={fadeIn} />
       <Button title="FadeOut" onPress={fadeOut} />
