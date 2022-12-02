acdce90b30b8e2aea59a439ecd892e2f3c9e40d7
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-12-01 23:51:37 -0300

    feat: FadeIn and FadeOut added

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,12 +1,33 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, { useRef } from 'react';
-import { View, StyleSheet, Animated } from 'react-native';
+import { View, StyleSheet, Animated, Button } from 'react-native';
 
 export const Animation101Screen = () => {
-  const opacity = useRef(new Animated.Value(0.4)).current;
+  const opacity = useRef(new Animated.Value(0)).current;
+
+  const fadeIn = () => {
+    Animated.timing(opacity, {
+      toValue: 0.4,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const fadeOut = () => {
+    Animated.timing(opacity, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
 
   return (
     <View style={styles.container}>
-      <Animated.View style={{ ...styles.purpleBox, opacity }} />
+      <Animated.View
+        style={{ ...styles.purpleBox, marginBottom: 20, opacity }}
+      />
+      <Button title="FadeIn" onPress={fadeIn} />
+      <Button title="FadeOut" onPress={fadeOut} />
     </View>
   );
 };
