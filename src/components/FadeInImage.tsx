809d21193a2fcaec8eb551668419ef983ceb291c
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-12-05 20:39:29 -0300

    feat: React Native Components finished

--- src/components/FadeInImage.tsx
+++ src/components/FadeInImage.tsx
@@ -1,14 +1,21 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, { useState } from 'react';
-import { Animated, View, ActivityIndicator } from 'react-native';
+import {
+  Animated,
+  View,
+  ActivityIndicator,
+  StyleProp,
+  ImageStyle,
+} from 'react-native';
 
 import { useAnimation } from '../hooks/useAnimation';
 
 interface Props {
   uri: string;
+  style?: StyleProp<ImageStyle>;
 }
 
-export const FadeInImage = ({ uri }: Props) => {
+export const FadeInImage = ({ uri, style }: Props) => {
   const { opacity, fadeIn } = useAnimation();
 
   const [isLoading, setIsLoading] = useState(true);
@@ -30,7 +37,7 @@ export const FadeInImage = ({ uri }: Props) => {
       <Animated.Image
         source={{ uri }}
         onLoadEnd={finishLoading}
-        style={{ width: '100%', height: 400, opacity }}
+        style={{ ...(style as any), opacity }}
       />
     </View>
   );
--- src/screens/InfiniteScrollScreen.tsx
+++ src/screens/InfiniteScrollScreen.tsx
@@ -10,7 +10,12 @@ export const InfiniteScrollScreen = () => {
   const [numbers, setNumbers] = useState([0, 1, 2, 3, 4, 5]);
 
   const renderItem = (item: number) => {
-    return <FadeInImage uri={`https://picsum.photos/id/${item}/500/400`} />;
+    return (
+      <FadeInImage
+        uri={`https://picsum.photos/id/${item}/500/400`}
+        style={{ width: '100%', height: 400 }}
+      />
+    );
   };
 
   const loadMore = () => {
@@ -29,7 +34,11 @@ export const InfiniteScrollScreen = () => {
         data={numbers}
         keyExtractor={item => item.toString()}
         renderItem={({ item }) => renderItem(item)}
-        ListHeaderComponent={<HeaderTitle title="Infinite Scroll" />}
+        ListHeaderComponent={() => (
+          <View style={{ marginHorizontal: 20 }}>
+            <HeaderTitle title="Infinite Scroll" />
+          </View>
+        )}
         onEndReached={loadMore}
         onEndReachedThreshold={0.5}
         ListFooterComponent={() => (
